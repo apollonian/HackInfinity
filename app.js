a6a404948e2b47a8947ca3838cbc6ee268fa399b
Jay Shah <jayshah.9596@gmail.com>
2017-11-12 07:22:11 +0530

    [Pv0.1.1]

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var fs = require('fs');
 var fetch = require('node-fetch');
 var request = require('request');
 
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
-
 var cloudinary = require('cloudinary');
 cloudinary.config({
     cloud_name: 'apollonian',
@@ -78,118 +75,42 @@ bot.dialog('getImage', [
         session.say("Send a photo of your crop");
         var msg = session.message;
         if (msg.attachments.length) {
-    
+
             // Message with attachment, proceed to download it.
             // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
             var attachment = msg.attachments[0];
 
-            // console.log(attachment)
-
             request(attachment.contentUrl, function (error, response, body) {
                 console.log('error:', error); // Print the error if one occurred
                 console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                cloudinary.uploader.upload("'" + body + "'", function (result) {
+                cloudinary.uploader.upload('./images/ex.jpg', (result) => {
                     console.log(result)
+                    let resultClassifier = runClassifier(result.url);
+                    console.log(resultClassifier)
                 });
             });
-
         }
     }
 
-
-    // (session, response) => {
-    //     var msg = session.message;
-    //     // Check for an attachment
-    //     if (msg.attachments.length) {
-    //         var attachment = msg.attachments[0]
-    //         // Echo back attachment
-    //         // decodeBase64Image(msg.attachments[0]);
-
-    //         session.send({
-    //             text: "You uploaded: ",
-    //             attachments: [{
-    //                 contentType: attachment.contentType,
-    //                 contentUrl: attachment.contentUrl,
-    //                 name: attachment.name
-    //             }]
-    //         });
-
-    //         // End the dialog and send a confirmation message
-    //         session.send('Please wait till the response arrives...');
-
-    //         // var bucketParams = {
-    //         //     // Body: './images/example.jpg',
-    //         //     Body: attachment.contentUrl,
-    //         //     Bucket: "hackinfinity",
-    //         //     Key: attachment.name,
-    //         //     ServerSideEncryption: "AES256"
-    //         // };
-
-    //         // console.log(bucketParams)
-
-    //         // s3.putObject(bucketParams, (err, data) => {
-    //         //     if (err) console.log(err, err.stack);   // an error occurred
-    //         //     else console.log(data);                 // successful response
-
-    //         // });
-
-    //         console.log(attachment)
-
-    //         request(attachment.contentUrl,
-    //             (response) => {
-
-    //                 // Send reply with attachment type & size
-    //                 var reply = new builder.Message(session)
-    //                     .text('Attachment of %s type and size of %s bytes received.', attachment.contentType, response.length);
-    //                 session.send(reply);
-
-    //             }
-    //         );
-
-
-    //         cloudinary.uploader.upload(decodeBase64Image(attachment.contentUrl), function (result) {
-    //             console.log(result)
-    //         });
-
-    //     } else {
-
-    //         // IF there is no attachment, reprompt
-    //         var reply = new builder.Message(session).text('Upload the picture of diseased crop.');
-    //         session.send(reply);
-    //     }
-    // }
 ])
 
+runClassifier = (url) => {
+    var options = {
+        method: 'GET',
+        url: 'https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify',
+        qs:
+            {
+                api_key: 'a998c221a94ed21a8c6d5aadf86dfa1e68ffed7b',
+                url: url,
+                version: '2016-05-20',
+                classifier_ids: 'DiseaseClassifier_910587994'
+            },
+        headers: { 'cache-control': 'no-cache' }
+    };
 
-
-function decodeBase64Image(dataString) {
-    let response = {}
-    response.type = 'image/jpg';
-    response.data = new Buffer(dataString, 'base64');
-    return response.data;
-}
-
-runClassifier = (attachment) => {
-    //     // var image = new png();
-    //     // var src = "data:image/png;base64,";
-    //     // src += attachment.contentUrl;
-    //     // image.src = src;
-    //     // var buffer = new Buffer(attachment.contentUrl, 'base64')
-    //     // fs.writeFile("./images/example.png", buffer, {encoding: 'base64'});
-
-    let params = {
-        images_file: attachment
-
-    }
-    visual_recognition.classify(params, (err, res) => {
-        if (!err) {
-            return res
-        } else {
-            return err
-        }
+    request(options, (error, response, body) => {
+        if (error) throw new Error(error);
+        console.log(body)
+        return (body);
     });
 }
-
-function getPhotoAttachment(session) {
-
-};
